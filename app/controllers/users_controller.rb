87dd71581e5f469d5f86f9c75c72af938459a751
Alex Gilbert <agilbert7911@gmail.com>
2014-07-31 22:53:53 -0400

    Added user functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
    
+   def index
+      @users = User.all
+   end
+
+   def show
+      @user = User.find(params[:id])
+   end
+
    def new
    end
 
    def create
-      render plain: params[:user].inspect
+      @user = User.create(user_params)
+      redirect_to @user
    end
+
+  private
+     def user_params
+       params.require(:user).permit(:username, :password, :password_confirm)
+     end
 
 end
